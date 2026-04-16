9dafca7f7ce70607de796bace4760f1c4c98807f
AndresMira21 <andres.miranda0630@gmail.com>
2026-04-16 10:51:31 -0500

    feat: subiendo models, views, templates, urls, forms de la app asistencia

--- taller_formularios_nombre_estudiantes/asistencia/models.py
+++ taller_formularios_nombre_estudiantes/asistencia/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Asistencia(models.Model):
+    full_name = models.CharField(max_length=150)
+    id = models.CharField(max_length=20)
+    email = models.EmailField()
+    date = models.DateField()
+    entry_time = models.TimeField()
+    exit_time = models.TimeField()
+    present = models.BooleanField(default=False)
+    observations = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.full_name} - {self.id} - ${self.observations}"
+    
+    
+    
