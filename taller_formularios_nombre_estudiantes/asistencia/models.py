958aa55e1355a3941a48a9e8340aac41a28521b4
AndresMira21 <andres.miranda0630@gmail.com>
2026-04-16 12:14:40 -0500

    feat: subiendo admin y cambiando form a español

--- taller_formularios_nombre_estudiantes/asistencia/models.py
+++ taller_formularios_nombre_estudiantes/asistencia/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Asistencia(models.Model):
-    full_name = models.CharField(max_length=150)
+    Nombre = models.CharField(max_length=150)
     id = models.CharField(max_length=20, primary_key=True)
-    email = models.EmailField()
-    date = models.DateField()
-    entry_time = models.TimeField()
-    exit_time = models.TimeField()
-    present = models.BooleanField(default=False)
-    observations = models.TextField(blank=True)
+    corero = models.EmailField()
+    fecha = models.DateField()
+    hora_entrada = models.TimeField()
+    hora_salida = models.TimeField()
+    presente = models.BooleanField(default=False)
+    observationes = models.TextField(blank=True)
 
     def __str__(self):
-        return f'{self.full_name} - {self.id} - {self.observations}'
+        return f'{self.Nombre} - {self.id} - {self.observationes}'
 
